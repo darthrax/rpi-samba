f3923e3d0df3e08541d927c5f78fa6a896d93a8d
darthrax <darthrax@users.noreply.github.com>
2021-02-10 23:45:34 +0530

    Update build_image.sh

--- build_image.sh
+++ build_image.sh
@@ -1,2 +1,4 @@
 #!/bin/bash
-docker build --no-cache -t dastrasmue/rpi-samba:v3 .
+docker buildx create --use
+docker buildx build --no-cache --push --platform="linux/amd64,linux/arm64/v8,linux/arm/v7" -t darthrax/rpi-samba:latest .
+docker buildx rm
